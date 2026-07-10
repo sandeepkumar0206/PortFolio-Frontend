f91cd09a9696430648942df72574f31b9f0e84d9
Sandeep Kumar <sandeepkumar073113@gmail.com>
2026-07-11 02:32:43 +0530

    Initial Setup and Chores

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/home/home.component').then((m) => m.HomeComponent),
+    title: 'Portfolio',
+  },
+  { path: '**', redirectTo: '' },
+];
